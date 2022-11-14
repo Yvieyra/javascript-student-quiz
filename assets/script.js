ca1545493a725ac49bcf1d0a460343ae9589cbcb
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-14 01:38:20 -0700

    added reset.css file, added code to JS file, and changed file path in HTML file

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var timeLeft = document.querySelector("#time-left")
+var timer = document.querySelector("#timer")
+
+var startButton = document.querySelector("#start-btn");
+
+var questionChoices = document.querySelector("question-choices")
+var question = document.querySelector("#question")
+var button1 = document.querySelector("#btn1")
+var button2 = document.querySelector("#btn2")
+var button3 = document.querySelector("#btn3")
+var button4 = document.querySelector("#btn4")
+var lineBreak = document.querySelector("#line-break")
+var answer = document.querySelector("#answer")
+
+var allDone = document.querySelector("#all-done")
+var finalScore = document.querySelector("#final-score")
+var initialsInput = document.querySelector("#intials") 
+
+var highScores = document.querySelector("#high-scores")
+var highestScore = document.querySelector("#score-results")
+
+var backButton = document.querySelector("#back-btn")
+var clearButton = document.querySelector("#clear-btn")
+
+//add questions and choices with answers in jS
