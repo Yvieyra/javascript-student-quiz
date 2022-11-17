3a82b6cb8b03d1cc8a1e62882f2c09b6fa088d1a
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-17 00:21:03 -0700

    succesfully added questions to my html when quiz starts

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var buttonChoice = document.querySelector(".button-choice")
 var startButton = document.querySelector("#start-btn");
 
 var questionChoices = document.querySelector("question-choices")
-var question = document.querySelector("#question")
+var questionHeader = document.querySelector("#question-header")
 var button1 = document.querySelector("#btn1")
 var button2 = document.querySelector("#btn2")
 var button3 = document.querySelector("#btn3")
@@ -23,73 +23,89 @@ var backButton = document.querySelector("#back-btn")
 var clearButton = document.querySelector("#clear-btn")
 
 var timerInterval;
-var secondsLeft = 10;
-//setinterval() function
+var secondsLeft = 60;
 
-//clearinterval() function
+
 //for loop to insert object properties into HTML
 // for (var i = 0; i < questions.length; i++) {
 
 // }
 
 function countDown() {
- timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timeLeft.textContent = secondsLeft;
 
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
-            clearInterval(timeLeft);
-            // Calls function to create and append image
+            clearInterval(timerInterval);
+            // Calls function to create a message
             sendMessage();
         }
+        //messsage function
         function sendMessage() {
             timeLeft.textContent = " Times up! ";
-          }   
+        }
     }, 1000);
 
-       
+    starterQuestion();
+
 }
+
+
 //eventlisteners
-startButton.addEventListener("click", countDown);
+startButton.addEventListener("click", countDown);//starts time at beginning of test and populates first question sets
+//eventlistener function
+function nextQuestion() { // is a next question necessary?}
+    if (questions[0].answer) {
+        answer.textContent = ("correct!");
+    
+}}
 
-// buttonChoice.addEventListener("click", function () {
-//     questions.question1
-// });
+buttonChoice.addEventListener("click", nextQuestion);// user selects questions choice and function should move to next questiion and give correct answer
 
+
+function starterQuestion() {
+    questionHeader.textContent = questions[0].question;
+    button1.textContent = questions[0].choices[0];
+    button2.textContent = questions[0].choices[1];
+    button3.textContent = questions[0].choices[2];
+    button4.textContent = questions[0].choices[3];
+    
+}
 
 // //get stored values for high scores
 
-// //add questions and choices with answers in jS (An array of Objects)
-// // one object per question in one big array
-// var questions = [
+//add questions and choices with answers in jS (An array of Objects)
+// one object per question in one big array
+var questions = [
 
-//     {
-//         question1: "To combine a message with a variable is called a: ",
-//         choices1: ["concatenation", "boolean", "string", "array"],
-//         answer1: "concatenation"
-//     },
-//     {
-//         question2: "A string is a series of characters and is surrounded by:  ",
-//         choices2: ["p tags", "brackets", "quotes", "parenthesis"],
-//         answer3: "quotes"
-//     },
-//     {
-//         question3: "The logical comparison operator modulus(%) is used for what?",
-//         choices3: ["percentage", "It returns the remainder between two numbers", "average", "reverses boolean value"],
-//         answer3: "It returns the remainder between two numbers"
-//     },
-//     {
-//         question4: "What does the following method do? .push()",
-//         choices4: ["inserts the parameter onto the end of an array and returns the new array order", "It re-assigns the array value to the only parameter value", "inserts the parameter to the start of an array and returns the new array order", "It returns the value of the parameter inside the push parenthessis if it exists in the array"
-//         ],
-//         answer4: "inserts the parameter onto the end of an array and returns the new array order"
-//     },
-//     {
-//         question5: "Why do we use for-loops?",
-//         choices5: ["to code quicker", "to execute a code more than once", "to find false statements", "to find true statements"],
-//         answer5: "It execute a code more than once"
-//     }
-// ]
+    {
+        question: "To combine a message with a variable is called a: ",
+        choices: ["boolean", "concatenation", "string", "array"],
+        answer: "concatenation"
+    },
+    {
+        question: "A string is a series of characters and is surrounded by:  ",
+        choices: ["p tags", "brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "The logical comparison operator modulus(%) is used for what?",
+        choices: ["percentage", "It returns the remainder between two numbers", "average", "reverses boolean value"],
+        answer: "It returns the remainder between two numbers"
+    },
+    {
+        question: "What does the following method do? .push()",
+        choices: ["inserts the parameter onto the end of an array and returns the new array order", "It re-assigns the array value to the only parameter value", "inserts the parameter to the start of an array and returns the new array order", "It returns the value of the parameter inside the push parenthessis if it exists in the array"
+        ],
+        answer: "inserts the parameter onto the end of an array and returns the new array order"
+    },
+    {
+        question: "Why do we use for-loops?",
+        choices: ["to code quicker", "to execute a code more than once", "to find false statements", "to find true statements"],
+        answer: "It execute a code more than once"
+    }
+]
 
 // Tests to check correct answers vs. wrong answers
